a90f6a88cd5204afdcbb27c32dd6e61072d84668
dawityifter <dawityifter@@gmail.com>
2023-09-02 14:56:50 -0500

    Basic Spring batch service that imports data from a CSV spreadsheet, transforms it with custom code, and stores the final results in a database.

---
+++ src/test/java/com/dawityifter/learning/springbatchdemo/SpringBatchDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dawityifter.learning.springbatchdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
